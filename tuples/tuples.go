51024c680b209ad69430b537466bda7edef0ee6d
Sam Dyer <samueljdyer@gmail.com>
2022-06-17 21:02:51 -0400

    Exported x, y, z, w in tuples. Refactored package to match.

--- tuples/tuples.go
+++ tuples/tuples.go
@@ -19,11 +19,11 @@ func Equals(a, b float64) bool {
 //Type Tuple provides the basic three-dimensional scalars
 //and a field that designates whether the tuple is a point or a vector.
 type Tuple struct {
-	x, y, z, w float64
+	X, Y, Z, W float64
 }
 
 func (t Tuple) IsPoint() bool {
-	if t.w == 1.0 {
+	if t.W == 1.0 {
 		return true
 	} else {
 		return false
@@ -39,10 +39,10 @@ func Vector(x, y, z float64) Tuple {
 }
 
 func TupleEquals(a Tuple, b Tuple) bool {
-	if Equals(a.x, b.x) &&
-		Equals(a.y, b.y) &&
-		Equals(a.z, b.z) &&
-		Equals(a.w, b.w) {
+	if Equals(a.X, b.X) &&
+		Equals(a.Y, b.Y) &&
+		Equals(a.Z, b.Z) &&
+		Equals(a.W, b.W) {
 		return true
 	} else {
 		return false
@@ -52,10 +52,10 @@ func TupleEquals(a Tuple, b Tuple) bool {
 func Add(nums ...Tuple) Tuple {
 	newTup := Tuple{0.0, 0.0, 0.0, 0.0}
 	for _, num := range nums {
-		newTup.x += num.x
-		newTup.y += num.y
-		newTup.z += num.z
-		newTup.w += num.w
+		newTup.X += num.X
+		newTup.Y += num.Y
+		newTup.Z += num.Z
+		newTup.W += num.W
 	}
 
 	return newTup
@@ -63,44 +63,44 @@ func Add(nums ...Tuple) Tuple {
 
 func Subtract(a, b Tuple) Tuple {
 	newTup := Tuple{}
-	newTup.x = a.x - b.x
-	newTup.y = a.y - b.y
-	newTup.z = a.z - b.z
-	newTup.w = a.w - b.w
+	newTup.X = a.X - b.X
+	newTup.Y = a.Y - b.Y
+	newTup.Z = a.Z - b.Z
+	newTup.W = a.W - b.W
 
 	return newTup
 }
 
 func (t *Tuple) Negate() {
-	t.x *= -1
-	t.y *= -1
-	t.z *= -1
-	t.w *= -1
+	t.X *= -1
+	t.Y *= -1
+	t.Z *= -1
+	t.W *= -1
 }
 
 func (t *Tuple) Multiply(scalar float64) {
-	t.x *= scalar
-	t.y *= scalar
-	t.z *= scalar
-	t.w *= scalar
+	t.X *= scalar
+	t.Y *= scalar
+	t.Z *= scalar
+	t.W *= scalar
 }
 
 func (t *Tuple) Divide(divisor float64) {
 	if divisor == 0.0 {
-		t.x = 0.0
-		t.y = 0.0
-		t.z = 0.0
-		t.w = 0.0
+		t.X = 0.0
+		t.Y = 0.0
+		t.Z = 0.0
+		t.W = 0.0
 	} else {
-		t.x /= divisor
-		t.y /= divisor
-		t.z /= divisor
-		t.w /= divisor
+		t.X /= divisor
+		t.Y /= divisor
+		t.Z /= divisor
+		t.W /= divisor
 	}
 }
 
 func (t *Tuple) Magnitude() float64 {
-	mag := math.Sqrt(math.Pow(t.x, 2) + math.Pow(t.y, 2) + math.Pow(t.z, 2))
+	mag := math.Sqrt(math.Pow(t.X, 2) + math.Pow(t.Y, 2) + math.Pow(t.Z, 2))
 	return mag
 }
 
@@ -108,19 +108,19 @@ func (t *Tuple) Normalize() Tuple {
 	norm := Tuple{}
 	mag := t.Magnitude()
 
-	norm.x = t.x / mag
-	norm.y = t.y / mag
-	norm.z = t.z / mag
-	norm.w = t.w
+	norm.X = t.X / mag
+	norm.Y = t.Y / mag
+	norm.Z = t.Z / mag
+	norm.W = t.W
 
 	return norm
 }
 
 func Dot(a, b Tuple) float64 {
-	dot := a.x*b.x +
-		a.y*b.y +
-		a.z*b.z +
-		a.w*b.w
+	dot := a.X*b.X +
+		a.Y*b.Y +
+		a.Z*b.Z +
+		a.W*b.W
 
 	return dot
 }
@@ -128,10 +128,10 @@ func Dot(a, b Tuple) float64 {
 func Cross(a, b Tuple) Tuple {
 	cross := Tuple{}
 
-	cross.x = a.y*b.z - a.z*b.y
-	cross.y = a.z*b.x - a.x*b.z
-	cross.z = a.x*b.y - a.y*b.x
-	cross.w = 0.0
+	cross.X = a.Y*b.Z - a.Z*b.Y
+	cross.Y = a.Z*b.X - a.X*b.Z
+	cross.Z = a.X*b.Y - a.Y*b.X
+	cross.W = 0.0
 
 	return cross
 }
--- tuples/tuples_test.go
+++ tuples/tuples_test.go
@@ -8,7 +8,7 @@ import (
 
 func TestPoint(t *testing.T) {
 	var testPoints = []struct {
-		x, y, z float64
+		X, Y, Z float64
 	}{
 		{0.0, 0.0, 0.0},
 		{4.3, -4.2, 3.1},
@@ -16,19 +16,19 @@ func TestPoint(t *testing.T) {
 	}
 
 	for _, testPt := range testPoints {
-		testName := fmt.Sprintf("%v, %v, %v", testPt.x, testPt.y, testPt.z)
+		testName := fmt.Sprintf("%v, %v, %v", testPt.X, testPt.Y, testPt.Z)
 		t.Run(testName, func(t *testing.T) {
-			point := Point(testPt.x, testPt.y, testPt.z)
-			if point.w != 1.0 {
+			point := Point(testPt.X, testPt.Y, testPt.Z)
+			if point.W != 1.0 {
 				t.Errorf("Wrong w")
 			}
-			if point.x != testPt.x {
+			if point.X != testPt.X {
 				t.Errorf("Wrong x")
 			}
-			if point.y != testPt.y {
+			if point.Y != testPt.Y {
 				t.Errorf("Wrong y")
 			}
-			if point.z != testPt.z {
+			if point.Z != testPt.Z {
 				t.Errorf("Wrong z")
 			}
 		})
@@ -37,7 +37,7 @@ func TestPoint(t *testing.T) {
 
 func TestVector(t *testing.T) {
 	var testVectors = []struct {
-		x, y, z float64
+		X, Y, Z float64
 	}{
 		{0.0, 0.0, 0.0},
 		{4.3, -4.2, 3.1},
@@ -45,19 +45,19 @@ func TestVector(t *testing.T) {
 	}
 
 	for _, testVec := range testVectors {
-		testName := fmt.Sprintf("%v, %v, %v", testVec.x, testVec.y, testVec.z)
+		testName := fmt.Sprintf("%v, %v, %v", testVec.X, testVec.Y, testVec.Z)
 		t.Run(testName, func(t *testing.T) {
-			point := Vector(testVec.x, testVec.y, testVec.z)
-			if point.w != 0.0 {
+			point := Vector(testVec.X, testVec.Y, testVec.Z)
+			if point.W != 0.0 {
 				t.Errorf("Wrong w")
 			}
-			if point.x != testVec.x {
+			if point.X != testVec.X {
 				t.Errorf("Wrong x")
 			}
-			if point.y != testVec.y {
+			if point.Y != testVec.Y {
 				t.Errorf("Wrong y")
 			}
-			if point.z != testVec.z {
+			if point.Z != testVec.Z {
 				t.Errorf("Wrong z")
 			}
 		})
@@ -78,8 +78,8 @@ func TestTupleEquals(t *testing.T) {
 
 	for _, testTup := range testTuples {
 		testName := fmt.Sprintf("Comparing Tuple {%v, %v, %v, %v} to Tuple {%v, %v, %v, %v} for equality (Expected Result: %v)",
-			testTup.a.x, testTup.a.y, testTup.a.z, testTup.a.w,
-			testTup.b.x, testTup.b.y, testTup.b.z, testTup.b.w,
+			testTup.a.X, testTup.a.Y, testTup.a.Z, testTup.a.W,
+			testTup.b.X, testTup.b.Y, testTup.b.Z, testTup.b.W,
 			testTup.ok)
 		t.Run(testName, func(t *testing.T) {
 			if TupleEquals(testTup.a, testTup.b) != testTup.ok {
@@ -100,9 +100,9 @@ func TestAdd(t *testing.T) {
 
 	for _, testTup := range testTuples {
 		testName := fmt.Sprintf("Adding Tuple {%v, %v, %v, %v} to Tuple {%v, %v, %v, %v} (Expected Result Tuple {%v, %v, %v, %v})",
-			testTup.a.x, testTup.a.y, testTup.a.z, testTup.a.w,
-			testTup.b.x, testTup.b.y, testTup.b.z, testTup.b.w,
-			testTup.result.x, testTup.result.y, testTup.result.z, testTup.result.w)
+			testTup.a.X, testTup.a.Y, testTup.a.Z, testTup.a.W,
+			testTup.b.X, testTup.b.Y, testTup.b.Z, testTup.b.W,
+			testTup.result.X, testTup.result.Y, testTup.result.Z, testTup.result.W)
 		t.Run(testName, func(t *testing.T) {
 			if !TupleEquals(Add(testTup.a, testTup.b), testTup.result) {
 				t.Errorf("Cannot correctly add tuples using standalone function!")
@@ -122,9 +122,9 @@ func TestSubtract(t *testing.T) {
 
 	for _, testTup := range testTuples {
 		testName := fmt.Sprintf("Subtracting Tuple {%v, %v, %v, %v} from Tuple {%v, %v, %v, %v} (Expected Result Tuple {%v, %v, %v, %v})",
-			testTup.a.x, testTup.a.y, testTup.a.z, testTup.a.w,
-			testTup.b.x, testTup.b.y, testTup.b.z, testTup.b.w,
-			testTup.result.x, testTup.result.y, testTup.result.z, testTup.result.w)
+			testTup.a.X, testTup.a.Y, testTup.a.Z, testTup.a.W,
+			testTup.b.X, testTup.b.Y, testTup.b.Z, testTup.b.W,
+			testTup.result.X, testTup.result.Y, testTup.result.Z, testTup.result.W)
 		t.Run(testName, func(t *testing.T) {
 			if !TupleEquals(Subtract(testTup.a, testTup.b), testTup.result) {
 				t.Errorf("Cannot correctly subtract tuples using standalone function!")
@@ -145,9 +145,9 @@ func TestNegate(t *testing.T) {
 
 	for _, testTup := range testTuples {
 		testName := fmt.Sprintf("Negating Tuple {%v, %v, %v, %v} (Expected Result Tuple {%v, %v, %v, %v})",
-			testTup.a.x, testTup.a.y, testTup.a.z, testTup.a.w,
+			testTup.a.X, testTup.a.Y, testTup.a.Z, testTup.a.W,
 
-			testTup.result.x, testTup.result.y, testTup.result.z, testTup.result.w)
+			testTup.result.X, testTup.result.Y, testTup.result.Z, testTup.result.W)
 		t.Run(testName, func(t *testing.T) {
 			testTup.a.Negate()
 			if !TupleEquals(testTup.a, testTup.result) {
@@ -170,9 +170,9 @@ func TestMultiply(t *testing.T) {
 
 	for _, testTup := range testTuples {
 		testName := fmt.Sprintf("Multiplying Tuple {%v, %v, %v, %v} by Scalar %v (Expected Result Tuple {%v, %v, %v, %v})",
-			testTup.a.x, testTup.a.y, testTup.a.z, testTup.a.w,
+			testTup.a.X, testTup.a.Y, testTup.a.Z, testTup.a.W,
 			testTup.scalar,
-			testTup.result.x, testTup.result.y, testTup.result.z, testTup.result.w)
+			testTup.result.X, testTup.result.Y, testTup.result.Z, testTup.result.W)
 		t.Run(testName, func(t *testing.T) {
 			testTup.a.Multiply(testTup.scalar)
 			if !TupleEquals(testTup.a, testTup.result) {
@@ -184,9 +184,9 @@ func TestMultiply(t *testing.T) {
 
 func TestDivide(t *testing.T) {
 	var testTuples = []struct {
-		a      Tuple
+		a       Tuple
 		divisor float64
-		result Tuple
+		result  Tuple
 	}{
 		{Tuple{1.0, -2.0, 3.0, -4.0}, 0.5, Tuple{2.0, -4.0, 6.0, -8.0}},
 		{Tuple{1.0, -2.0, 3.0, -4.0}, 2.0, Tuple{0.5, -1.0, 1.5, -2.0}},
@@ -196,9 +196,9 @@ func TestDivide(t *testing.T) {
 
 	for _, testTup := range testTuples {
 		testName := fmt.Sprintf("Dividing Tuple {%v, %v, %v, %v} by divisor %v (Expected Result Tuple {%v, %v, %v, %v})",
-			testTup.a.x, testTup.a.y, testTup.a.z, testTup.a.w,
+			testTup.a.X, testTup.a.Y, testTup.a.Z, testTup.a.W,
 			testTup.divisor,
-			testTup.result.x, testTup.result.y, testTup.result.z, testTup.result.w)
+			testTup.result.X, testTup.result.Y, testTup.result.Z, testTup.result.W)
 		t.Run(testName, func(t *testing.T) {
 			testTup.a.Divide(testTup.divisor)
 			if !TupleEquals(testTup.a, testTup.result) {
@@ -222,13 +222,13 @@ func TestMagnitude(t *testing.T) {
 
 	for _, testTup := range testTuples {
 		testName := fmt.Sprintf("Computing Magnitude for Tuple {%v, %v, %v, %v} (Expected Result %v)",
-			testTup.a.x, testTup.a.y, testTup.a.z, testTup.a.w,
+			testTup.a.X, testTup.a.Y, testTup.a.Z, testTup.a.W,
 			testTup.result)
 		t.Run(testName, func(t *testing.T) {
 			mag := testTup.a.Magnitude()
 			if !Equals(mag, testTup.result) {
 				t.Errorf("Cannot correctly compute Magnitude for tuples using standalone function! Tuple: {%v,%v,%v,%v}; Calculated magnitude: %v; Expected Magnitude: %v",
-					testTup.a.x, testTup.a.y, testTup.a.z, testTup.a.w, mag, testTup.result)
+					testTup.a.X, testTup.a.Y, testTup.a.Z, testTup.a.W, mag, testTup.result)
 			}
 		})
 	}
@@ -244,13 +244,13 @@ func TestNormalize(t *testing.T) {
 
 	for _, testTup := range testTuples {
 		testName := fmt.Sprintf("Normalizing Tuple {%v, %v, %v, %v} (Expected Result Tuple {%v, %v, %v, %v})",
-			testTup.a.x, testTup.a.y, testTup.a.z, testTup.a.w,
-			testTup.result.x, testTup.result.y, testTup.result.z, testTup.result.w)
+			testTup.a.X, testTup.a.Y, testTup.a.Z, testTup.a.W,
+			testTup.result.X, testTup.result.Y, testTup.result.Z, testTup.result.W)
 		t.Run(testName, func(t *testing.T) {
 			normed := testTup.a.Normalize()
 			if !TupleEquals(normed, testTup.result) {
 				t.Errorf("Cannot correctly Normalize tuples using standalone function! Tuple values are {%v, %v, %v, %v}",
-					normed.x, normed.y, normed.z, normed.w)
+					normed.X, normed.Y, normed.Z, normed.W)
 			}
 		})
 	}
@@ -266,8 +266,8 @@ func TestDot(t *testing.T) {
 
 	for _, testTup := range testTuples {
 		testName := fmt.Sprintf("Dotting Tuple {%v, %v, %v, %v} with Tuple {%v, %v, %v, %v}, Expected Product: %v",
-			testTup.a.x, testTup.a.y, testTup.a.z, testTup.a.w,
-			testTup.b.x, testTup.b.y, testTup.b.z, testTup.b.w,
+			testTup.a.X, testTup.a.Y, testTup.a.Z, testTup.a.W,
+			testTup.b.X, testTup.b.Y, testTup.b.Z, testTup.b.W,
 			testTup.result)
 		t.Run(testName, func(t *testing.T) {
 			dotProduct := Dot(testTup.a, testTup.b)
@@ -289,13 +289,13 @@ func TestCross(t *testing.T) {
 
 	for _, testTup := range testTuples {
 		testName := fmt.Sprintf("Crossing Tuple {%v, %v, %v, %v} with Tuple {%v, %v, %v, %v}",
-			testTup.a.x, testTup.a.y, testTup.a.z, testTup.a.w,
-			testTup.b.x, testTup.b.y, testTup.b.z, testTup.b.w)
+			testTup.a.X, testTup.a.Y, testTup.a.Z, testTup.a.W,
+			testTup.b.X, testTup.b.Y, testTup.b.Z, testTup.b.W)
 		t.Run(testName, func(t *testing.T) {
 			crossProduct := Cross(testTup.a, testTup.b)
 			if !TupleEquals(crossProduct, testTup.result) {
 				t.Errorf("Cannot correctly Dot tuples using standalone function! Cross product: Tuple{%v,%v,%v,%v}",
-					crossProduct.x, crossProduct.y, crossProduct.z, crossProduct.w)
+					crossProduct.X, crossProduct.Y, crossProduct.Z, crossProduct.W)
 			}
 		})
 	}
