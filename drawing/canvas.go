0bd3e508174a0d49286a873548bf74ff30597d22
Sam Dyer <samueljdyer@gmail.com>
2022-07-17 11:59:38 -0400

    Removed CreateCanvas. CanvasEquals tests passing.

--- drawing/canvas.go
+++ drawing/canvas.go
@@ -9,28 +9,14 @@ type Canvas struct {
 	pixels        [][]Color
 }
 
-func (canvas *Canvas) CreateCanvas(height, width int, pixels [][]Color) error {
-	if pixels == nil {
-		canvas.pixels = make([][]Color, height)
-		for i, _ := range canvas.pixels {
-			canvas.pixels[i] = make([]Color, width)
-			for j := 0; j < len(canvas.pixels[i]); j++ {
-				canvas.WritePixel(Color{0.0, 0.0, 0.0}, Position{i, j})
-			}
-		}
-	} else {
-		canvas.height = len(pixels)
-		canvas.width = len(pixels[0])
-		canvas.pixels = pixels
-	}
-	return nil
-}
-
-func CanvasEquals(a, b Canvas) bool {
+func CanvasEquals(a, b *Canvas) bool {
 	if a.height != b.height {
 		return false
 	}
 	if a.width != b.width {
+		return false
+	}
+	if a.pixels == nil || b.pixels == nil {
 		return false
 	}
 	for i := 0; i < len(a.pixels); i++ {
--- drawing/canvas_test.go
+++ drawing/canvas_test.go
@@ -5,34 +5,34 @@ import (
 )
 
 func TestCanvasEquals(t *testing.T) {
-
-}
-
-func TestCreateCanvas(t *testing.T) {
 	black := Color{1.0, 1.0, 1.0}
 	red := Color{1.0, 0.0, 0.0}
 	var testCanvas = []struct {
-		len, wid int
-		result   Canvas
+		A, B            Canvas
+		result          bool
+		testDescription string
 	}{
-		{4, 5, Canvas{4, 4, nil}},
-		{3, 2, Canvas{3, 2, [][]Color{
+		{Canvas{4, 4, nil}, Canvas{4, 4, nil}, false, "Two canvases with uninitalized pixel fields cannot be equal"},
+		{Canvas{3, 2, [][]Color{
 			{red, black, red},
 			{black, red, black},
 			{red, black, red},
-		}}},
+		}}, Canvas{3, 2, nil}, false, "Test that different canvases are not equal"},
+		{Canvas{3, 2, [][]Color{
+			{red, black, red},
+			{black, red, black},
+			{red, black, red},
+		}}, Canvas{3, 2, [][]Color{
+			{red, black, red},
+			{black, red, black},
+			{red, black, red},
+		}}, true, "Identical canvases are equal"},
 	}
 
 	for _, testC := range testCanvas {
-		t.Run("Compute Hadamard product of Colors", func(t *testing.T) {
-			canvas := Canvas{}
-			err := canvas.CreateCanvas(testC.len, testC.wid, [][]Color{
-				{red, black, red},
-				{black, red, black},
-				{red, black, red},
-			})
-			if err != nil && !CanvasEquals(canvas, testC.result) {
-				t.Errorf("Not working! Canvases are not equal")
+		t.Run(testC.testDescription, func(t *testing.T) {
+			if !(CanvasEquals(&testC.A, &testC.B) == testC.result) {
+				t.Errorf("Not working! Failure: %v", testC.testDescription)
 			}
 		})
 	}
