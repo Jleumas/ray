84dc8f35a253681cd5d8e404136d09e5dd765144
Sam Dyer <samueljdyer@gmail.com>
2022-07-17 11:32:18 -0400

    Working test for CreateCanvas

--- drawing/canvas.go
+++ drawing/canvas.go
@@ -26,6 +26,24 @@ func (canvas *Canvas) CreateCanvas(height, width int, pixels [][]Color) error {
 	return nil
 }
 
+func CanvasEquals(a, b Canvas) bool {
+	if a.height != b.height {
+		return false
+	}
+	if a.width != b.width {
+		return false
+	}
+	for i := 0; i < len(a.pixels); i++ {
+		for j := 0; j < len(a.pixels[0]); j++ {
+			if !ColorEquals(a.pixels[i][j], b.pixels[i][j]) {
+				return false
+			}
+		}
+	}
+
+	return true
+}
+
 func (c *Canvas) WritePixel(col Color, pos Position) {
 	c.pixels[pos.X][pos.Y] = col
 }
--- drawing/canvas_test.go
+++ drawing/canvas_test.go
@@ -4,18 +4,36 @@ import (
 	"testing"
 )
 
+func TestCanvasEquals(t *testing.T) {
+
+}
+
 func TestCreateCanvas(t *testing.T) {
 	black := Color{1.0, 1.0, 1.0}
-	red:= Color{1.0, 0.0, 0.0}
+	red := Color{1.0, 0.0, 0.0}
 	var testCanvas = []struct {
 		len, wid int
 		result   Canvas
 	}{
 		{4, 5, Canvas{4, 4, nil}},
 		{3, 2, Canvas{3, 2, [][]Color{
-			{black, black, black},
-			{black, black, black},
-			{black, black, black}
+			{red, black, red},
+			{black, red, black},
+			{red, black, red},
 		}}},
+	}
+
+	for _, testC := range testCanvas {
+		t.Run("Compute Hadamard product of Colors", func(t *testing.T) {
+			canvas := Canvas{}
+			err := canvas.CreateCanvas(testC.len, testC.wid, [][]Color{
+				{red, black, red},
+				{black, red, black},
+				{red, black, red},
+			})
+			if err != nil && !CanvasEquals(canvas, testC.result) {
+				t.Errorf("Not working! Canvases are not equal")
+			}
+		})
 	}
 }
